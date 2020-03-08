e7411d2978b4fe281631aedbc7b64411b803d0d4
Dell <Dell@SaadMansuri.mshome.net>
2020-03-09 00:53:11 +0530

    first commit

---
+++ Test_ART_ID/src/main/java/Test_GroupID/Test_ART_ID/App.java
@@ -0,0 +1,13 @@
+package Test_GroupID.Test_ART_ID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
