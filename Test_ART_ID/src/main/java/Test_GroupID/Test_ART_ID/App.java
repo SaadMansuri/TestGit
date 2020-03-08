79e758f8a21de25241b5902577bfe0633fe276ff
Dell <Dell@SaadMansuri.mshome.net>
2020-03-09 01:13:12 +0530

    cvxcv

--- Test_ART_ID/src/main/java/Test_GroupID/Test_ART_ID/App.java
+++ Test_ART_ID/src/main/java/Test_GroupID/Test_ART_ID/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello    dGit! " );
         System.out.println( "Hello     dGit! " );
-        System.out.println( "Hello     dGit! " );
-    }
+        System.out.println( "Hello      dGit! " );
+    } 
 }
